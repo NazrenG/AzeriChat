ecb702ef87c8b447c2bf20ea4c32cfd7b2024331
NazrenG <nezrin.qu@gmail.com>
2023-10-25 01:34:17 +0400

    azeriChat design

---
+++ Class/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzeriChat.Class
+{
+    public class Message
+    {
+        public string? Title { get; set; }
+        public string? Time { get; set; }
+
+        public Message()
+        {
+            Title = null;
+            Time = null;
+        }
+        public Message(string text, string time)
+        {
+            Title = text;
+            Time = time;
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using AzeriChat.Class;
+
+namespace AzeriChat
+{
+    public partial class MainWindow : Window
+    {
+       public  List<Message> messages { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            messages = new List<Message>()
+            {
+                new()
+                {
+                    Title="Hi!",
+                    Time="9:00"
+                },
+                new()
+                {
+                    Title="Hello",
+                    Time="9:05"
+                },
+                   new()
+                {
+                    Title="How are you?",
+                    Time="10:09"
+                },
+                new()
+                {
+                    Title=";)",
+                    Time="11:45"
+                },
+                   new()
+                {
+                    Title="asdfasfd",
+                    Time="12:00"
+                },
+      
+                
+            };
+            DataContext = this;
+        }
+
+    }
+}
